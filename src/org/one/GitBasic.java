bb74c809cf1b4dc73015b745fe8074a352fd3d03
keerthanak27311 <keerthanak27311@gmail.com>
2021-11-25 12:05:12 +0530

    my task

---
+++ src/org/one/GitBasic.java
@@ -0,0 +1,18 @@
+package org.one;
+
+
+public class GitBasic {
+	public static void main(String[] args) {
+		int a[]=new int[5];
+		a[0]=1;
+		a[1]=12;
+		a[2]=13;
+		a[3]=14;
+		a[4]=15;
+		System.out.println(a[3]);
+		
+	}
+
+	
+	
+}
